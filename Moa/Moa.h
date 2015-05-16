880387ea36eaa0c106b4463b4fd41537cb5b5988
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-05-16 17:37:41 +1000

    Initial Commit

---
+++ Moa/Moa.h
@@ -0,0 +1,19 @@
+//
+//  Moa.h
+//  Moa
+//
+//  Created by Evgenii on 16/05/2015.
+//  Copyright (c) 2015 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Moa.
+FOUNDATION_EXPORT double MoaVersionNumber;
+
+//! Project version string for Moa.
+FOUNDATION_EXPORT const unsigned char MoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Moa/PublicHeader.h>
+
+
