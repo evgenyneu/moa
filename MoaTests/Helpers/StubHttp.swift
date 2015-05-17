b751c693abbd8dcc9381bb2cb55b5b66e239afb9
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-05-17 18:50:53 +1000

    Handle error

--- MoaTests/Helpers/StubHttp.swift
+++ MoaTests/Helpers/StubHttp.swift
@@ -10,17 +10,43 @@ class StubHttp {
     withImage("yellow.png", forUrlPart: url)
   }
   
-  private class func withImage(imageName: String, forUrlPart url: String) {
-    OHHTTPStubs.stubRequestsPassingTest({
-      MoaString.contains($0.URL!.absoluteString!, substring: url)
-    }) { _ in
-//      let imagePath = OHPathForFile(imageName, self.dynamicType)!
-      
-      let imagePath = NSBundle(forClass: self).pathForResource(imageName, ofType: nil)!
-      
-      return OHHTTPStubsResponse(fileAtPath: imagePath,
-        statusCode: 200,
-        headers: ["Content-Type": "image/png"])
+  private class func requestUrlIncludes(urlPart:String) -> (NSURLRequest->Bool) {
+    return { req in
+      MoaString.contains(req.URL!.absoluteString!, substring: urlPart)
     }
+  }
+  
+  private class func fixture(filename: String,
+    responseHeaders: [NSObject : AnyObject]?,
+    statusCode: Int = 200,
+    responseTime: NSTimeInterval = 0) -> (NSURLRequest->OHHTTPStubsResponse) {
+      
+    let filePath = NSBundle(forClass: self).pathForResource(filename, ofType: nil)!
+    
+    return { _ in
+      
+      var response = OHHTTPStubsResponse(fileAtPath: filePath,
+        statusCode: Int32(statusCode), headers: responseHeaders)
+      
+      if responseTime > 0 {
+        response = response.requestTime(0, responseTime: responseTime)
+      }
+      
+      return response
+    }
+  }
+  
+  class func withImage(imageName: String,
+    forUrlPart urlPart: String,
+    responseHeaders: [NSObject : AnyObject]? = ["Content-Type": "image/png"],
+    statusCode: Int = 200,
+    responseTime: NSTimeInterval = 0) {
+      
+    OHHTTPStubs.stubRequestsPassingTest(requestUrlIncludes(urlPart),
+      withStubResponse: fixture(imageName,
+        responseHeaders: responseHeaders,
+        statusCode: statusCode,
+        responseTime: responseTime)
+      )
   }
 }
--- MoaTests/MoaTests.swift
+++ MoaTests/MoaTests.swift
@@ -34,4 +34,44 @@ class MoaTests: XCTestCase {
     let color = MoaImage.pixelColorAtImageCenter(image!)
     XCTAssertEqual(UIColor.yellowColor(), color)
   }
+  
+  func testLoadImage_ErrorWhenImageNotFound() {
+    StubHttp.withImage("yellow.png", forUrlPart: "ant.jpg", statusCode: 404)
+    let responseArrived = expectationWithDescription("response arrived")
+    
+    let moa = Moa()
+    moa.url = "http://evgenii.com/moa/ant.jpg"
+    
+    let timer = MoaTimer.runAfter(0.01) { timer in
+      responseArrived.fulfill()
+    }
+    
+    waitForExpectationsWithTimeout(1) { error in }
+    XCTAssert(moa.image == nil)
+  }
+  
+  func testLoadImage_ErrorWhenResponseIsNotImage() {
+    StubHttp.withImage("yellow.png",
+      forUrlPart: "ant.jpg",
+      responseHeaders: ["Content-Type": "text/html"])
+
+    let responseArrived = expectationWithDescription("response arrived")
+    
+    let moa = Moa()
+    moa.url = "http://evgenii.com/moa/ant.jpg"
+    
+    let timer = MoaTimer.runAfter(0.01) { timer in
+      responseArrived.fulfill()
+    }
+    
+    waitForExpectationsWithTimeout(1) { error in }
+    XCTAssert(moa.image == nil)
+  }
+  
+  func testCancellingDownload() {
+//    let moa = Moa()
+//    moa.url = "http://evgenii.com/moa/ant.jpg"
+    
+    
+  }
 }
