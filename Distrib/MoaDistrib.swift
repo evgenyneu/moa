eb7a70055e77f257076ca8cd362dd9ee642c0d94
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-06-06 18:25:40 +1000

    Supply correct cache path for OSX

--- Distrib/MoaDistrib.swift
+++ Distrib/MoaDistrib.swift
@@ -368,14 +368,35 @@ struct MoaHttpSession {
     
     configuration.requestCachePolicy = Moa.settings.cache.requestCachePolicy
     
+    #if os(iOS)
+      // Cache path is a directory name in iOS
+      let cachePath = Moa.settings.cache.diskPath
+    #elseif os(OSX)
+      // Cache path is a disk path in OSX
+      let cachePath = osxCachePath(Moa.settings.cache.diskPath)
+    #endif
+    
     let cache = NSURLCache(
       memoryCapacity: Moa.settings.cache.memoryCapacityBytes,
       diskCapacity: Moa.settings.cache.diskCapacityBytes,
-      diskPath: Moa.settings.cache.diskPath)
+      diskPath: cachePath)
     
     configuration.URLCache = cache
     
     return NSURLSession(configuration: configuration)
+  }
+  
+  // Returns the cache path for OSX.
+  private static func osxCachePath(dirName: String) -> String {
+    var basePath = NSTemporaryDirectory()
+    if let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.ApplicationSupportDirectory,
+      NSSearchPathDomainMask.UserDomainMask, true) as? [String]
+      where paths.count > 0 {
+        
+      basePath = paths[0]
+    }
+    
+    return basePath.stringByAppendingPathComponent(dirName)
   }
   
   static func cacheSettingsChanged(oldSettings: MoaSettingsCache) {
