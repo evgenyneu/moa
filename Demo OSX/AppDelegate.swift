340d6e97bba81e663369fb7b1462ef8d150c6ef3
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-06-07 14:38:01 +1000

    Max os x demo icons

---
+++ Demo OSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Demo OSX
+//
+//  Created by Evgenii on 7/06/2015.
+//  Copyright (c) 2015 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Demo OSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Demo OSX
+//
+//  Created by Evgenii on 7/06/2015.
+//  Copyright (c) 2015 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
