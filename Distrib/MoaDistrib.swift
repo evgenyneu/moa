55dd431642d5a7799d32fd53386bc29c46d75c8c
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-06-07 12:16:24 +1000

    Build for mac

--- Distrib/MoaDistrib.swift
+++ Distrib/MoaDistrib.swift
@@ -605,44 +605,48 @@ func !=(lhs: MoaSettingsCache, rhs: MoaSettingsCache) -> Bool {
 //
 // ----------------------------
 
-import WatchKit
+#if os(iOS)
 
-private var xoMoaInterfaceImageAssociationKey: UInt8 = 0
+  import WatchKit
 
-/**
+  private var xoMoaInterfaceImageAssociationKey: UInt8 = 0
 
-Interface image extension for downloading images.
-
-let interfaceImage = WKInterfaceImage()
-interfaceImage.moa.url = "http://site.com/image.jpg"
-
-*/
-public extension WKInterfaceImage {
-  
   /**
-  
-  Image download extension.
-  Assign its `url` property to download and show the image.
-  
+
+  Interface image extension for downloading images.
+
   let interfaceImage = WKInterfaceImage()
   interfaceImage.moa.url = "http://site.com/image.jpg"
-  
+
   */
-  public var moa: Moa {
-    get {
-      if let value = objc_getAssociatedObject(self, &xoMoaInterfaceImageAssociationKey) as? Moa {
-        return value
-      } else {
-        let moa = Moa(wkInterfaceImage: self)
-        objc_setAssociatedObject(self, &xoMoaInterfaceImageAssociationKey, moa, objc_AssociationPolicy(OBJC_ASSOCIATION_RETAIN))
-        return moa
+  public extension WKInterfaceImage {
+    
+    /**
+    
+    Image download extension.
+    Assign its `url` property to download and show the image.
+    
+    let interfaceImage = WKInterfaceImage()
+    interfaceImage.moa.url = "http://site.com/image.jpg"
+    
+    */
+    public var moa: Moa {
+      get {
+        if let value = objc_getAssociatedObject(self, &xoMoaInterfaceImageAssociationKey) as? Moa {
+          return value
+        } else {
+          let moa = Moa(wkInterfaceImage: self)
+          objc_setAssociatedObject(self, &xoMoaInterfaceImageAssociationKey, moa, objc_AssociationPolicy(OBJC_ASSOCIATION_RETAIN))
+          return moa
+        }
+      }
+      
+      set {
+        objc_setAssociatedObject(self, &xoMoaInterfaceImageAssociationKey, newValue, objc_AssociationPolicy(OBJC_ASSOCIATION_RETAIN))
       }
     }
-    
-    set {
-      objc_setAssociatedObject(self, &xoMoaInterfaceImageAssociationKey, newValue, objc_AssociationPolicy(OBJC_ASSOCIATION_RETAIN))
-    }
   }
-}
+
+#endif
 
 
