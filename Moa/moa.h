a3cce77b14c890b384557613bb02a552867820cf
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-07-27 10:46:35 +1000

    Rename verion number constant

--- Moa/moa.h
+++ Moa/moa.h
@@ -1,9 +1,9 @@
 #import <UIKit/UIKit.h>
 
-//! Project version number for moa.
+//! Project version number for Moa.
 FOUNDATION_EXPORT double MoaVersionNumber;
 
-//! Project version string for moa.
+//! Project version string for Moa.
 FOUNDATION_EXPORT const unsigned char MoaVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Moa/PublicHeader.h>
