9110afd023dfe593418d065213e999ff5b535f1c
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-07-27 10:32:12 +1000

    Fixing code sign

--- Moa/moa.h
+++ Moa/moa.h
@@ -1,10 +1,10 @@
 #import <UIKit/UIKit.h>
 
-//! Project version number for Moa.
-FOUNDATION_EXPORT double MoaVersionNumber;
+//! Project version number for moa.
+FOUNDATION_EXPORT double moaVersionNumber;
 
-//! Project version string for Moa.
-FOUNDATION_EXPORT const unsigned char MoaVersionString[];
+//! Project version string for moa.
+FOUNDATION_EXPORT const unsigned char moaVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Moa/PublicHeader.h>
 
