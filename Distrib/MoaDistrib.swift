d565ed9dbccdf99313c572acf36994e569f27b80
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-09-04 09:55:01 +1000

    Report error

--- Distrib/MoaDistrib.swift
+++ Distrib/MoaDistrib.swift
@@ -48,6 +48,41 @@ enum MoaError: ErrorType {
     default: return nil
     }
   }
+  
+  var localizedDescription: String {
+    let comment = "Moa image downloader error"
+  
+    switch self {
+    case .InvalidUrlString:
+      return NSLocalizedString("Invalid URL.", comment: comment)
+    
+    case .HttpStatusCodeIsNot200:
+      return NSLocalizedString("Response status code is not 200.", comment: comment)
+      
+    case .MissingResponseContentTypeHttpHeader:
+      return NSLocalizedString("Response header is missing content type.", comment: comment)
+      
+    case .NotAnImageContentTypeInResponseHttpHeader:
+      return NSLocalizedString("Response content type is not an image type. Content type needs to be  'image/jpeg', 'image/pjpeg' or 'image/png'", comment: comment)
+      
+    case .FailedToReadImageData:
+      return NSLocalizedString("Could not convert response data to an image format.", comment: comment)
+      
+    case .SimulatedError:
+      return NSLocalizedString("Test error.", comment: comment)
+    }
+  }
+  
+  var code: Int {
+    return (self as NSError).code
+  }
+  
+  var nsError: NSError {
+    return NSError(
+      domain: "MoaError",
+      code: code,
+      userInfo: [NSLocalizedDescriptionKey: localizedDescription])
+  }
 }
 
 
--- MoaTests/Http/MoaErrorTests.swift
+++ MoaTests/Http/MoaErrorTests.swift
@@ -35,4 +35,31 @@ class MoaErrorTests: XCTestCase {
     let result = MoaError.fromError(error)
     XCTAssert(result == nil)
   }
+  
+  func testGetNsError() {
+    var error = MoaError.InvalidUrlString.nsError
+    XCTAssertEqual("MoaError", error.domain)
+    XCTAssertEqual(0, error.code)
+    XCTAssertEqual("Invalid URL.", error.localizedDescription)
+    
+    error = MoaError.HttpStatusCodeIsNot200.nsError
+    XCTAssertEqual(1, error.code)
+    XCTAssertEqual("Response HTTP status code is not 200.", error.localizedDescription)
+    
+    error = MoaError.MissingResponseContentTypeHttpHeader.nsError
+    XCTAssertEqual(2, error.code)
+    XCTAssertEqual("Response HTTP header is missing content type.", error.localizedDescription)
+    
+    error = MoaError.NotAnImageContentTypeInResponseHttpHeader.nsError
+    XCTAssertEqual(3, error.code)
+    XCTAssertEqual("Response content type is not an image type. Content type needs to be  'image/jpeg', 'image/pjpeg' or 'image/png'", error.localizedDescription)
+    
+    error = MoaError.FailedToReadImageData.nsError
+    XCTAssertEqual(4, error.code)
+    XCTAssertEqual("Could not convert response data to an image format.", error.localizedDescription)
+    
+    error = MoaError.SimulatedError.nsError
+    XCTAssertEqual(5, error.code)
+    XCTAssertEqual("Test error.", error.localizedDescription)
+  }
 }
