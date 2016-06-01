4a55e01a7628a4ead91d066ae4731186e7024f14
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-06-01 17:38:07 +1000

    Add tvos support

---
+++ moa-tvOS/moa-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  moa-tvOS.h
+//  moa-tvOS
+//
+//  Created by Evgenii on 1/06/2016.
+//  Copyright © 2016 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for moa-tvOS.
+FOUNDATION_EXPORT double moa_tvOSVersionNumber;
+
+//! Project version string for moa-tvOS.
+FOUNDATION_EXPORT const unsigned char moa_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <moa_tvOS/PublicHeader.h>
+
+
