e2cfa501d5e95733168f3c9f5e6f4eed31161c09
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2018-09-19 21:43:23 +1000

    Update to Swift 4.2

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   let randomImageUrl = RandomImageUrl()
 
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]?) -> Bool {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
 
     return true
   }
